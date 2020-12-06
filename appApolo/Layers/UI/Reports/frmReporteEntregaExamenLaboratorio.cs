20bdcd56e347d926116baa9aa7d50d5639fc4c99
Luis Rodríguez <lalbertorodriguez96@gmail.com>
2020-12-06 02:13:13 -0600

    Full Tercera pantalla procesos

--- appApolo/Layers/UI/Reports/frmReporteEntregaExamenLaboratorio.cs
+++ appApolo/Layers/UI/Reports/frmReporteEntregaExamenLaboratorio.cs
@@ -53,10 +53,9 @@ namespace UTN.Winforms.Apolo.Layers.UI.Reports
 
             ReportParameter param = new ReportParameter("resultExam", this.ResultadoExamen);
             this.rptVisor.LocalReport.SetParameters(param);
-
             this.rptVisor.RefreshReport();
-
-            EnviaCorreoElectronico();
+            ExportarAPDF();
+            //EnviaCorreoElectronico();
         }
 
         public void EnviaCorreoElectronico()
@@ -64,21 +63,10 @@ namespace UTN.Winforms.Apolo.Layers.UI.Reports
             string ruta = @"c:\temp\reporte.pdf";
             try
             {
-                if (!Directory.Exists(@"c:\temp"))
-                    Directory.CreateDirectory(@"c:\temp");
-
-                byte[] Bytes = this.rptVisor.LocalReport.Render(format: "PDF", deviceInfo: "");
-
-                using (FileStream stream = new FileStream(ruta, FileMode.Create))
-                {
-                    stream.Write(Bytes, 0, Bytes.Length);
-                }
-
-
                 MailMessage mensaje = new MailMessage();
                 mensaje.IsBodyHtml = true;
-                mensaje.Subject = "Resultado de Exámen Laboratorio " + DescripcionExamen;
-                mensaje.Body = "    Estimado paciente de Laboratorio Clínico Apolo S.A,\nadjuntamos " +
+                mensaje.Subject = "Resultado de Exámen Laboratorio " + " " + DescripcionExamen;
+                mensaje.Body = "    Estimado paciente de Laboratorio Clínico Apolo S.A, adjuntamos " +
                                "resultado de su exámen de " + DescripcionExamen;
                 mensaje.From = new MailAddress("generictestlarh96@gmail.com");
                 mensaje.To.Add(this.EmailPaciente);
@@ -106,6 +94,49 @@ namespace UTN.Winforms.Apolo.Layers.UI.Reports
                 MessageBox.Show("Se ha producido el siguiente error: " + er.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
+        }
+
+        private void ExportarAPDF()
+        {   
+            try
+            {
+                string ruta = @"c:\temp\reporte.pdf";
+                if (!Directory.Exists(@"c:\temp"))
+                    Directory.CreateDirectory(@"c:\temp");
+
+                byte[] Bytes = this.rptVisor.LocalReport.Render(format: "PDF", deviceInfo: "");
+                File.WriteAllBytes(ruta, Bytes);
+
+                MailMessage mensaje = new MailMessage();
+                mensaje.IsBodyHtml = true;
+                mensaje.Subject = "Resultado de Exámen Laboratorio " + " " + DescripcionExamen;
+                mensaje.Body = "    Estimado paciente de Laboratorio Clínico Apolo S.A, adjuntamos " +
+                               "resultado de su exámen de " + DescripcionExamen;
+                mensaje.From = new MailAddress("generictestlarh96@gmail.com");
+                mensaje.To.Add(this.EmailPaciente);
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                smtp.Port = 587;
+                smtp.Credentials = new NetworkCredential("generictestlarh96@gmail.com", "C0mpl1c4d4");
+                smtp.EnableSsl = true;
+                Attachment attachment = new Attachment(ruta);
+                mensaje.Attachments.Add(attachment);
+                smtp.Send(mensaje);
+                MessageBox.Show("Correo Enviado");
+            }
+            catch (SqlException sqlError)
+            {
+                // Mensaje de Error
+                MessageBox.Show("Se ha producido el siguiente error: \n" + Utilitarios.GetCustomErrorByNumber(sqlError), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception er)
+            {
+
+                StringBuilder msg = new StringBuilder();
+                msg.AppendFormat(Utilitarios.CreateGenericErrorExceptionDetail(MethodBase.GetCurrentMethod(), er));
+                _MyLogControlEventos.ErrorFormat("Error {0}", msg.ToString());
+                // Mensaje de Error
+                MessageBox.Show("Se ha producido el siguiente error: " + er.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
